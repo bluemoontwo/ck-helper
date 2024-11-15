ddf7a5c35850e008d25e52af0b14859196c6d7d9
damie824 <admin@damie.works>
2024-11-15 22:41:54 +0900

    Edit store error # 1

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -11,5 +11,9 @@ npm install pnpm -g
 
 pnpm install
 
+# .store 디렉토리 생성 및 권한 설정
+mkdir -p /home/ubuntu/.store
+chown -R ubuntu:ubuntu /home/ubuntu/.store
+
 pm2 delete ckhelper || true
 pm2 start dist/index.js --name ckhelper
