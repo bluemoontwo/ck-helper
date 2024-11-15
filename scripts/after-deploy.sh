558a01071945c42eb1dfc5ca72d69d93f7da6bc0
damie824 <admin@damie.works>
2024-11-15 22:03:33 +0900

    edit after-deploy.sh #3

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -1,14 +1,14 @@
-export NVM_DIR="$HOME/ubuntu/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
 #!/bin/bash
 REPOSITORY=/home/ubuntu/build
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 cd $REPOSITORY
 
-sudo npm install pnpm -g
+npm install pnpm -g
 
-sudo pnpm install
+pnpm install
 
-sudo pm2 start dist/index.js --name ckhelper
+pm2 start dist/index.js --name ckhelper
