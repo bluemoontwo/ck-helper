e16046486e249a4d6ecb74c14e96b5c0afe92b3f
damie824 <admin@damie.works>
2024-11-15 22:01:54 +0900

    edit after-deploy.sh #2

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -1,3 +1,7 @@
+export NVM_DIR="$HOME/ubuntu/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 #!/bin/bash
 REPOSITORY=/home/ubuntu/build
 
